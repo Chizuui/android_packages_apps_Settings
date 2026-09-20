31e37fc74639c28f481a05c1db248478b3e9759b
Chizuui <desckun@gmail.com>
2026-09-20 14:02:22 +0000

    Settings: Add background blur toggle
    Change-Id: I1bc21da52d937376a7df6147a2740e2cea5624f7

--- src/com/android/settings/display/BackgroundBlurPreferenceController.java
+++ src/com/android/settings/display/BackgroundBlurPreferenceController.java
@@ -1,21 +1,11 @@
-/*
- * Copyright (C) 2026 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- */
-
 package com.android.settings.display;
 
 import android.content.Context;
 import android.provider.Settings;
 
-import androidx.preference.Preference;
-import androidx.preference.TwoStatePreference;
+import com.android.settings.core.TogglePreferenceController;
 
-import com.android.settings.core.BasePreferenceController;
-
-public class BackgroundBlurPreferenceController extends BasePreferenceController
-        implements Preference.OnPreferenceChangeListener {
+public class BackgroundBlurPreferenceController extends TogglePreferenceController {
 
     private static final String DISABLE_WINDOW_BLURS = "disable_window_blurs";
 
@@ -29,29 +19,24 @@ public class BackgroundBlurPreferenceController extends BasePreferenceController
     }
 
     @Override
-    public void updateState(Preference preference) {
-        super.updateState(preference);
-
-        if (preference instanceof TwoStatePreference) {
-            final boolean enabled = Settings.Global.getInt(
-                    mContext.getContentResolver(),
-                    DISABLE_WINDOW_BLURS,
-                    0) == 0;
-
-            ((TwoStatePreference) preference).setChecked(enabled);
-        }
+    public boolean isChecked() {
+        // 0 = blur enabled
+        // 1 = blur disabled
+        return Settings.Global.getInt(
+                mContext.getContentResolver(),
+                DISABLE_WINDOW_BLURS,
+                0
+        ) == 0;
     }
 
     @Override
-    public boolean onPreferenceChange(Preference preference, Object newValue) {
-        final boolean enabled = (Boolean) newValue;
-
-        // disable_window_blurs:
-        // 0 = blur enabled
-        // 1 = blur disabled
+    public boolean setChecked(boolean isChecked) {
+        // Switch ON  -> blur enabled  -> 0
+        // Switch OFF -> blur disabled -> 1
         return Settings.Global.putInt(
                 mContext.getContentResolver(),
                 DISABLE_WINDOW_BLURS,
-                enabled ? 0 : 1);
+                isChecked ? 0 : 1
+        );
     }
 }
