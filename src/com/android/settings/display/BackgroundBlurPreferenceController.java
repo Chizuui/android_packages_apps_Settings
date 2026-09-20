af917b6f6184ff35b00d5f0c150ab7c89f71ce5a
Chizuui <desckun@gmail.com>
2026-09-20 14:43:25 +0000

    Settings: Fix background blur preference controller
    Change-Id: I25629a0b2b8ebd4aa5807395a003ff0a5d95637b

--- src/com/android/settings/display/BackgroundBlurPreferenceController.java
+++ src/com/android/settings/display/BackgroundBlurPreferenceController.java
@@ -3,6 +3,7 @@ package com.android.settings.display;
 import android.content.Context;
 import android.provider.Settings;
 
+import com.android.settings.R;
 import com.android.settings.core.TogglePreferenceController;
 
 public class BackgroundBlurPreferenceController extends TogglePreferenceController {
@@ -20,23 +21,22 @@ public class BackgroundBlurPreferenceController extends TogglePreferenceControll
 
     @Override
     public boolean isChecked() {
-        // 0 = blur enabled
-        // 1 = blur disabled
         return Settings.Global.getInt(
                 mContext.getContentResolver(),
                 DISABLE_WINDOW_BLURS,
-                0
-        ) == 0;
+                0) == 0;
     }
 
     @Override
     public boolean setChecked(boolean isChecked) {
-        // Switch ON  -> blur enabled  -> 0
-        // Switch OFF -> blur disabled -> 1
         return Settings.Global.putInt(
                 mContext.getContentResolver(),
                 DISABLE_WINDOW_BLURS,
-                isChecked ? 0 : 1
-        );
+                isChecked ? 0 : 1);
+    }
+
+    @Override
+    public int getSliceHighlightMenuRes() {
+        return R.string.menu_key_display;
     }
 }
